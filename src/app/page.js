d2bfae1c4b4c310f5d0a774d4d0706788b11bac4
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2024-06-25 15:49:57 +0530

    input component and home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function page() {
-  return <div>Home Page</div>;
+  return (
+    <div className="max-w-xl mx-auto border-r border-l min-h-screen">
+      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+      </div>
+    </div>
+  );
 }
